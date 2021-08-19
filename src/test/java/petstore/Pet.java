161076d607ec756bf79fee00bf45bd235d397809
someone <someone@someplace.com>
2021-08-19 00:15:00 -0300

    some init msg

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -11,6 +11,7 @@ import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.is;
 
 // 3 - Classe
 public class Pet {
@@ -40,6 +41,8 @@ public class Pet {
         .then() //Então
                 .log().all()
                 .statusCode(200)
+                .body("name", is("Shailon"))
+                .body("status", is("available"))
         ;
     }
 }
